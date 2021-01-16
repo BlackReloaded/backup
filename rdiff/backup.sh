2634a4dea2f27e8c037ac15d986f478cf785deab
Marc Kohlbau <m.kohlbau@myopenfactory.com>
2021-01-16 22:49:59 +0100

    fix env vars error
    Signed-off-by: Marc Kohlbau <m.kohlbau@myopenfactory.com>

--- rdiff/backup.sh
+++ rdiff/backup.sh
@@ -1,24 +1,26 @@
 #!/bin/bash
 
+source ${HOME}/env.conf
+
 TARGET="${TARGET:-/root/backups}"
 OLD="${OLD:-6M}"
 
 cleanup() {
     echo "Cleanup rdiff-backup"
-    for src in $(echo $BACKUPS | tr ";" "\n"); do
+    for src in $(echo ${BACKUPS} | tr ";" "\n"); do
         dst="${src/::/}"
-        echo "Cleanup backup source $src to $TARGET/$dst"
-        rdiff-backup --remove-older-than $OLD "$TARGET/$dst"
+        echo "Cleanup backup source ${src} to ${TARGET}/${dst}"
+        rdiff-backup --remove-older-than ${OLD} "${TARGET}/${dst}"
     done
     echo "Cleanup complete"
 }
 
 create() {
     echo "Creating backup"
-    for src in $(echo $BACKUPS | tr ";" "\n"); do
+    for src in $(echo ${BACKUPS} | tr ";" "\n"); do
         dst="${src/::/}"
-        echo "Backup source $src to $TARGET/$dst"
-        rdiff-backup --print-statistics $src "$TARGET/$dst"
+        echo "Backup source ${src} to ${TARGET}/${dst}"
+        rdiff-backup --print-statistics ${src} "${TARGET}/${dst}"
         echo "Done"
     done
     echo "backup complete"
